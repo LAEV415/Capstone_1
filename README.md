5c81673167eb8c90b989c6f4035609c9a0499e7e
LAEV415 <laev415@gmail.com>
2026-04-27 22:39:44 -0700

    Updated README file, Created sales analysis file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Capstone_1
-Data Analyst tasked with identifying information related to sales territory using business' sales database
+As a Data Analyst for EmporiUm, a rapidly expanding student bookstore chain, I was tasked with extracting and analyzing territory-specific performance metrics. EmporiUm operates a hybrid retail model, offering tech products, art supplies, and educational materials through both brick-and-mortar storefronts and e-commerce platforms.
---
+++ espinosavillicana_sales_analysis.sql
@@ -0,0 +1,17 @@
+-- Assigned to analyze the Florida Sales Territory
+-- This Territory includes: (City, StoreId)
+-- 'Cape Canaveral',	'719'
+-- 'Fort Lauderdale',	'720'
+-- 'Jacksonville',  	'721'
+-- 'Key West',  		'722'
+-- 'Lakeland',  		'723'
+-- 'Miami',  			'724'
+-- 'Naples',  			'725'
+-- 'Orlando',  			'726'
+-- 'Sebring',  			'727'
+-- 'Tallahassee', 		'728'
+-- 'Tampa', 			'729'
+
+-- What is total revenue overall for sales in the assigned territory, plus the start date and end date
+-- that tell you what period the data covers?
+
